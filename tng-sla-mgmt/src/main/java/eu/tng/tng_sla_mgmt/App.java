9c4ac6bf1f872f1d8012e89efd4d8aa2cdf2a02b
Marios Touloupou <mtouloup@unipi.gr>
2018-02-24 17:57:24 +0200

    Empty Jenkins File

---
+++ tng-sla-mgmt/src/main/java/eu/tng/tng_sla_mgmt/App.java
@@ -0,0 +1,13 @@
+package eu.tng.tng_sla_mgmt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
