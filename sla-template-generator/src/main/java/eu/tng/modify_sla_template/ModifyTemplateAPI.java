eb6d85278a64fca54aba7811e18023648331456e
Evgenia Kapassa <ekapassa@unipi.gr>
2018-03-29 17:22:35 +0200

    alignment with REST best practices

--- sla-template-generator/src/main/java/eu/tng/modify_sla_template/ModifyTemplateAPI.java
+++ sla-template-generator/src/main/java/eu/tng/modify_sla_template/ModifyTemplateAPI.java
@@ -31,6 +31,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
@@ -44,18 +45,19 @@ import org.json.simple.JSONObject;
 public class ModifyTemplateAPI {
 
 	/**
-	 * api call in order to modify an already existing sla template mendatory input
+	 * api call in order to edit an already existing sla template mendatory input
 	 * parameters from the user: uuid, field, old_value, value.
-	 * http://localhost:8080/tng-sla-mgmt/slas/templates?uuid=<>&field=<>&old-value=<>&value=<>
+	 * http://localhost:8080/tng-sla-mgmt/api/v3/slas/edit/stemplates/{sla_uuid}?field=<>&old_value=<>&value=<>
 	 * 
 	 */
 
 	@SuppressWarnings("static-access")
-    @GET
+	@GET
+	@Path("/{sla_uuid}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response ModifyTemplate(@Context UriInfo info) {
+	public Response ModifyTemplate(@PathParam("sla_uuid") String sla_uuid, @Context UriInfo info) {
 
-		String uuid = info.getQueryParameters().getFirst("uuid");
+		// String uuid = info.getQueryParameters().getFirst("uuid");
 		String field = info.getQueryParameters().getFirst("field");
 		String old_value = info.getQueryParameters().getFirst("old_value");
 		String value = info.getQueryParameters().getFirst("value");
@@ -63,7 +65,7 @@ public class ModifyTemplateAPI {
 		String field_to_edit = (field + "=" + old_value);
 
 		Sla_Editor se = new Sla_Editor();
-		String new_sla_uuid = se.Edit_value(uuid, field_to_edit, value);
+		String new_sla_uuid = se.Edit_value(sla_uuid, field_to_edit, value);
 
 		Get_Sla_Template mt = new Get_Sla_Template();
 		JSONObject edited_template = mt.Get_Sla(new_sla_uuid);
@@ -71,10 +73,17 @@ public class ModifyTemplateAPI {
 		return Response.status(200).entity(edited_template).build();
 	}
 
+	/**
+	 * api call in order to modify an already existing sla template by adding new
+	 * slo mendatory input parameters from the user: uuid, field, old_value, value.
+	 * http://localhost:8080/tng-sla-mgmt/api/v3/slas/edit/stemplates/modify/{uuid}?objectives=[]&slo_value=[]&slo_definition=[]&slo_unit=[]&metric=[]&expression=[]&expression_unit=[]&rate=[]&parameter_name=[]&parameter_value=[]&parameter_definition=[]&parameter_unit=[]
+	 * 
+	 */
+
 	@GET
-	@Path("/customize")
-	public Response CustomizeTemplate(@QueryParam("objectives") List<String> objectives,
-			@QueryParam("uuid") String sla_uuid, @QueryParam("slo_value") List<String> slo_value,
+	@Path("/modify/{sla_uuid}")
+	public Response CustomizeTemplate(@PathParam("sla_uuid") String sla_uuid,
+			@QueryParam("objectives") List<String> objectives, @QueryParam("slo_value") List<String> slo_value,
 			@QueryParam("slo_value") List<String> slo_definition, @QueryParam("slo_unit") List<String> slo_unit,
 			@QueryParam("metric") List<String> metric, @QueryParam("expression") List<String> expression,
 			@QueryParam("expression_unit") List<String> expression_unit, @QueryParam("rate") List<String> rate,
@@ -84,14 +93,15 @@ public class ModifyTemplateAPI {
 			@QueryParam("parameter_name") List<String> parameter_name) {
 
 		@SuppressWarnings("unused")
-        Sla_Editor se = new Sla_Editor();
-		String new_uuid = Sla_Editor.Add_Fields(sla_uuid, objectives, slo_value, slo_definition, slo_unit,
-				metric, expression, expression_unit, rate,parameter_unit,parameter_definition,parameter_value,parameter_name);
-		
+		Sla_Editor se = new Sla_Editor();
+		String new_uuid = Sla_Editor.Add_Fields(sla_uuid, objectives, slo_value, slo_definition, slo_unit, metric,
+				expression, expression_unit, rate, parameter_unit, parameter_definition, parameter_value,
+				parameter_name);
+
 		Get_Sla_Template mt = new Get_Sla_Template();
-        JSONObject modified_template = mt.Get_Sla(new_uuid);
-        
+		JSONObject modified_template = mt.Get_Sla(new_uuid);
+
 		return Response.status(200).entity(modified_template).build();
-		
+
 	}
 }
