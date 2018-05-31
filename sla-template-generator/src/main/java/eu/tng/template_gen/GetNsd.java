00def2e71525ec85530f26804ad534c408299c27
Evgenia Kapassa <ekapassa@unipi.gr>
2018-05-31 16:38:36 +0300

    create template with multiple guarantee ids

--- sla-template-generator/src/main/java/eu/tng/template_gen/GetNsd.java
+++ sla-template-generator/src/main/java/eu/tng/template_gen/GetNsd.java
@@ -42,82 +42,81 @@ import org.json.simple.parser.ParseException;
 
 public class GetNsd {
 
-    public void getNSD(String nsId) {
-        Nsd setNsdFields = new Nsd();
-        ArrayList<String> mon_desc_list = new ArrayList<String>();
-        ArrayList<String> mon_metric_list = new ArrayList<String>();
-        ArrayList<String> mon_unit_list = new ArrayList<String>();
+	public void getNSD(String nsId) {
+		Nsd setNsdFields = new Nsd();
+		ArrayList<String> mon_desc_list = new ArrayList<String>();
+		ArrayList<String> mon_metric_list = new ArrayList<String>();
+		ArrayList<String> mon_unit_list = new ArrayList<String>();
 
-        try {
-            URL url = new URL("http://pre-int-sp-ath.5gtango.eu:4011/catalogues/api/v2/network-services/" + nsId);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestProperty("Content-Type", "application/json");
+		try {
+			URL url = new URL("http://pre-int-sp-ath.5gtango.eu:4011/catalogues/api/v2/network-services/" + nsId);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestProperty("Content-Type", "application/json");
 
-            if (conn.getResponseCode() != 200) {
-                System.out.println("Failed : HTTP error code : NSD not FOUND");
-            }
+			if (conn.getResponseCode() != 200) {
+				System.out.println("Failed : HTTP error code : NSD not FOUND");
+			}
 
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-            String output;
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			String output;
 
-            while ((output = br.readLine()) != null) {
-                JSONParser parser = new JSONParser();
-                try {
+			while ((output = br.readLine()) != null) {
+				JSONParser parser = new JSONParser();
+				try {
 
-                    Object obj = parser.parse(output);
-                    JSONObject jsonObject = (JSONObject) obj;
-                    if (jsonObject.containsKey("nsd")) {
-                        JSONObject nsd = (JSONObject) jsonObject.get("nsd");
+					Object obj = parser.parse(output);
+					JSONObject jsonObject = (JSONObject) obj;
+					if (jsonObject.containsKey("nsd")) {
+						JSONObject nsd = (JSONObject) jsonObject.get("nsd");
 
-                        // get nsd name
-                        String name = (String) nsd.get("name");
-                        setNsdFields.setName(name);
-                        
-                        // get nsd vendor
-                        String vendor = (String) nsd.get("vendor");
-                        setNsdFields.setVendor(vendor);
-                        
-                        // get nsd version
-                        String version = (String) nsd.get("version");
-                        setNsdFields.setVersion(version);
+						// get nsd name
+						String name = (String) nsd.get("name");
+						setNsdFields.setName(name);
 
+						// get nsd vendor
+						String vendor = (String) nsd.get("vendor");
+						setNsdFields.setVendor(vendor);
 
-                        // get nsd description
-                        String description = (String) nsd.get("description");
-                        setNsdFields.setDescription(description);
+						// get nsd version
+						String version = (String) nsd.get("version");
+						setNsdFields.setVersion(version);
 
-                        // get monitoring_parameters-desc,metric,unit
-                        if (nsd.containsKey("monitoring_parameters")) {
-                            JSONArray monitoring_parameters = (JSONArray) nsd.get("monitoring_parameters");
-                            for (int i = 0; i < monitoring_parameters.size(); i++) {
-                                JSONObject monitoring_parameter = (JSONObject) monitoring_parameters.get(i);
-                                mon_desc_list.add((String) monitoring_parameter.get("desc"));
-                                mon_metric_list.add((String) monitoring_parameter.get("metric"));
-                                mon_unit_list.add((String) monitoring_parameter.get("unit"));
-                            }
-                            setNsdFields.SetMonDesc(mon_desc_list);
-                            setNsdFields.SetMonMetric(mon_metric_list);
-                            setNsdFields.SetMonUnit(mon_unit_list);
+						// get nsd description
+						String description = (String) nsd.get("description");
+						setNsdFields.setDescription(description);
 
-                        }
-                    }
+						// get monitoring_parameters-desc,metric,unit
+						if (nsd.containsKey("monitoring_parameters")) {
+							JSONArray monitoring_parameters = (JSONArray) nsd.get("monitoring_parameters");
+							for (int i = 0; i < monitoring_parameters.size(); i++) {
+								JSONObject monitoring_parameter = (JSONObject) monitoring_parameters.get(i);
+								mon_desc_list.add((String) monitoring_parameter.get("desc"));
+								mon_metric_list.add((String) monitoring_parameter.get("metric"));
+								mon_unit_list.add((String) monitoring_parameter.get("unit"));
+							}
+							setNsdFields.SetMonDesc(mon_desc_list);
+							setNsdFields.SetMonMetric(mon_metric_list);
+							setNsdFields.SetMonUnit(mon_unit_list);
 
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
+						}
+					}
 
-            }
-            conn.disconnect();
-        } catch (MalformedURLException e) {
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
 
-            e.printStackTrace();
+			}
+			conn.disconnect();
+		} catch (MalformedURLException e) {
 
-        } catch (IOException e) {
+			e.printStackTrace();
 
-            e.printStackTrace();
+		} catch (IOException e) {
 
-        }
+			e.printStackTrace();
 
-    }
+		}
+
+	}
 
 }
