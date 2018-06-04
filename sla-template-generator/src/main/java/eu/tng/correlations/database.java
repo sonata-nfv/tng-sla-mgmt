bddf550712a1b840efdec4cbbfa36bccbe5d9504
Evgenia Kapassa <ekapassa@unipi.gr>
2018-06-04 13:03:01 +0300

    Correlations initial commit - postgreSQL crud operations

---
+++ sla-template-generator/src/main/java/eu/tng/correlations/database.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2017 5GTANGO, UPRC ALL RIGHTS RESERVED.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Neither the name of the 5GTANGO, UPRC nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without specific prior
+ * written permission.
+ * 
+ * This work has been performed in the framework of the 5GTANGO project, funded by the European
+ * Commission under Grant number 761493 through the Horizon 2020 and 5G-PPP programmes. The authors
+ * would like to acknowledge the contributions of their colleagues of the 5GTANGO partner consortium
+ * (www.5gtango.eu).
+ *
+ * @author Evgenia Kapassa (MSc), UPRC
+ * 
+ * @author Marios Touloupou (MSc), UPRC
+ * 
+ */
+
+package eu.tng.correlations;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class database {
+
+	static Connection c = null;
+	static Statement stmt = null;
+
+	/**
+	 * Connect to PostgreSQL
+	 */
+	public static void connectPostgreSQL() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/sla-manager", "postgres", "admin");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened sla-manager database successfully");
+	}
+
+	/**
+	 * Create table if not exist
+	 */
+	public static void createTableNSTemplate() {
+		try {
+			stmt = c.createStatement();
+			String sql = "CREATE TABLE ns_template" + "(ID  SERIAL PRIMARY KEY," + " NS_UUID CHAR(50) NOT NULL, "
+					+ "SLA_UUID  CHAR(50)  NOT NULL )";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			// table already exists
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Table ns_template created successfully");
+	}
+
+	/**
+	 * Insert operation
+	 */
+	public static void insertRecord(String tablename, String ns_uuid, String sla_uuid) {
+		try {
+			c.setAutoCommit(false);
+			Statement stmt = c.createStatement();
+			String sql = "INSERT INTO " + tablename + " (NS_UUID,SLA_UUID) " + "VALUES (" + ns_uuid + "," + sla_uuid
+					+ ");";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.commit();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Records created successfully");
+	}
+
+	/**
+	 * Delete Record
+	 */
+	public static void deleteRecord(String tablename, String sla_uuid) {
+		Statement stmt = null;
+		try {
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+			String sql = "DELETE from " + tablename + " where SLA_UUID=" + sla_uuid + ";";
+			stmt.executeUpdate(sql);
+			c.commit();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Records with sla_uuid:" + sla_uuid + " deleted successfully");
+	}
+
+	/**
+	 * Select all records
+	 */
+	public static void selectAllRecords(String tablename) {
+		Statement stmt = null;
+		try {
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM " + tablename + ";");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String ns_uuid = rs.getString("ns_uuid");
+				String sla_uuid = rs.getString("sla_uuid");
+				System.out.println("ID = " + id);
+				System.out.println("NS_UUID = " + ns_uuid);
+				System.out.println("SLA_UUID = " + sla_uuid);
+			}
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Operation done successfully");
+	}
+
+	/**
+	 * Close connection with PostgreSQL
+	 */
+	public static void closePostgreSQL() {
+		try {
+			c.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
