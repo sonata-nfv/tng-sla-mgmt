396246fe73dead922441f53f4c29415ba2ec1d4e
Evgenia Kapassa <ekapassa@unipi.gr>
2018-06-28 12:37:11 +0300

    create monitoring rule stracture

--- sla-template-generator/src/main/java/eu/tng/rules/MonitoringRules.java
+++ sla-template-generator/src/main/java/eu/tng/rules/MonitoringRules.java
@@ -28,23 +28,80 @@
 
 package eu.tng.rules;
 
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class MonitoringRules {
-	
+
 	/**
 	 * Create monitoring rules based on a instantiated NS and the selected SLA
 	 **/
-	
-	public static void main(String[] args){
-		createMonitroingRules("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee");
-	}
-	
-	public static JSONObject createMonitroingRules(String sla_uuid) {
-		
+	public static JSONObject createMonitroingRules(String sla_uuid, ArrayList<String> vnfrs_list,
+			ArrayList<String> vdus_list) {
+
+		ArrayList<String> slo_list = new ArrayList<String>();
+		slo_list.add("A");
+
 		System.out.println(sla_uuid);
-		return null;		
+		System.out.println(vnfrs_list);
+		System.out.println(vdus_list);
+
+		if (sla_uuid != null) {
+			/**
+			 * Create the rules
+			 **/
+			// ** root **/
+			JSONObject root = new JSONObject();
+
+			// ** sla id field **/
+			root.put("sla_cnt", sla_uuid);
+
+			// ** vnfs array **/
+			JSONArray vnfs = new JSONArray();
+			for (int i = 0; i < vnfrs_list.size(); i++) {
+				JSONObject nvfid = new JSONObject();
+				nvfid.put("nvfid", vnfrs_list.get(i));
+				vnfs.add(nvfid);
+
+				// ** vdus array **/
+				JSONArray vdus = new JSONArray();
+				for (int j = 0; j < vdus_list.size(); j++) {
+					JSONObject vduid = new JSONObject();
+					vduid.put("vdu_id", vdus_list.get(j));
+					vdus.add(vduid);
+
+					// ** rules array **/
+					JSONArray rules = new JSONArray();
+					// JSONObject rule = new JSONObject();
+					for (int k = 0; k < slo_list.size(); k++) {
+						JSONObject rule = new JSONObject();
+						rule.put("name", "sla:rule:" + "name");
+						rule.put("duration", "duration");
+						rule.put("description", "description");
+						rule.put("condition", "condition");
+						JSONObject notification_type = new JSONObject();
+						notification_type.put("id", "2");
+						notification_type.put("type", "rabbitmq");
+						rule.put("notification_type", notification_type);
+						
+						rules.add(rule);
+					}
+
+					vduid.put("rules", rules);
+				}
+				nvfid.put("vdus", vdus);
+
+			}
+			root.put("vnfs", vnfs);
+			System.out.println("root ==> " + root);
+
+		} else {
+			System.out.println("ERROR: Unable to create rules. SLA ID is null");
+		}
+
+		return null;
 	}
-	
 
 }
