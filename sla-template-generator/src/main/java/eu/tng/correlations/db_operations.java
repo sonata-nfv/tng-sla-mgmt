3854db3ce8bd8062df10253075247e050bce4f6d
Marios <touloupos@gmail.com>
2018-07-12 11:22:56 +0300

    Minor typo fix

--- sla-template-generator/src/main/java/eu/tng/correlations/db_operations.java
+++ sla-template-generator/src/main/java/eu/tng/correlations/db_operations.java
@@ -100,7 +100,7 @@ public class db_operations {
         try {
             stmt = c.createStatement();
             String sql = "CREATE TABLE IF NOT EXISTS cust_sla" + "(ID  SERIAL PRIMARY KEY," + " NS_UUID TEXT NOT NULL, "
-            		+ "NSI_UUID TEXT NULL" + "NS_NAME TEXT NOT NULL," + "SLA_UUID  TEXT NOT NULL," + "SLA_NAME TEXT NOT NULL,"
+            		+ "NSI_UUID TEXT NULL," + "NS_NAME TEXT NOT NULL," + "SLA_UUID  TEXT NOT NULL," + "SLA_NAME TEXT NOT NULL,"
                     + "SLA_DATE TIMESTAMPTZ DEFAULT Now()," + "SLA_STATUS TEXT NOT NULL," + "CUST_EMAIL TEXT NOT NULL,"
                     + "CUST_UUID  TEXT NOT NULL," + "INST_ID TEXT NOT NULL," + "INST_STATUS  TEXT NOT NULL )";
             stmt.executeUpdate(sql);
