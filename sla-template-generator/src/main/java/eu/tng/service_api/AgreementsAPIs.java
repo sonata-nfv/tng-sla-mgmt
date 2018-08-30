c5259f4e2f67c8e81818e331dee73de9f6b2afb4
Evgenia Kapassa <ekapassa@unipi.gr>
2018-08-30 12:31:00 +0300

    content=length fix

--- sla-template-generator/src/main/java/eu/tng/service_api/AgreementsAPIs.java
+++ sla-template-generator/src/main/java/eu/tng/service_api/AgreementsAPIs.java
@@ -294,7 +294,7 @@ public class AgreementsAPIs {
 			existingTemplates = agreement;
 
 			apiresponse = Response.ok((Object) existingTemplates);
-			apiresponse.header("Content-Length", agreement.toJSONString().length());
+			apiresponse.header("Content-Length", agreement.toJSONString().length()-8);
 			return apiresponse.status(200).build();
 
 		} catch (Exception e) {
