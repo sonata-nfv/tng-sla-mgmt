22d24d4aa4bdc02ddd636eb850768e494790bde3
Evgenia Kapassa <ekapassa@unipi.gr>
2018-11-22 02:32:39 +0200

    Updating log4j.xml

--- sla-template-generator/src/main/java/eu/tng/service_api/templatesAPIs.java
+++ sla-template-generator/src/main/java/eu/tng/service_api/templatesAPIs.java
@@ -76,7 +76,7 @@ import eu.tng.correlations.*;
 @Consumes(MediaType.APPLICATION_JSON)
 public class templatesAPIs {
 
-	static Logger logger = LogManager.getLogger("SLAM_Logger");
+	final Logger logger = LogManager.getLogger("SLAM_Logger");
 	
 	/**
 	 * api call in order to get a list with all the existing sla templates
@@ -401,15 +401,4 @@ public class templatesAPIs {
 
 	}
 	
-	public static void main(String [] args)
-	{
-		ThreadContext.put("type", "I");
-		ThreadContext.put("operation", "Get SLA Templates");
-		ThreadContext.put("status", "200");
-		logger.info("Available SLA Templates4");
-		logger.error("Available SLA Templates5");
-		logger.warn("Available SLA Templates6");
-		ThreadContext.clearAll();
-	}
-
 }
