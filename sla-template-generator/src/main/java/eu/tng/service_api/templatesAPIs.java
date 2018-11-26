7652fd1bcd89e119b4a45703ff81d190163abce6
Evgenia Kapassa <ekapassa@unipi.gr>
2018-11-26 15:28:06 +0200

    update create template API to include licensing fields

--- sla-template-generator/src/main/java/eu/tng/service_api/templatesAPIs.java
+++ sla-template-generator/src/main/java/eu/tng/service_api/templatesAPIs.java
@@ -228,17 +228,12 @@ public class templatesAPIs {
 		ArrayList<String> guarantees = new ArrayList<String>();
 		guarantees.addAll(formParams.get("guaranteeId"));
 
-		// logging
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		String timestamps = timestamp.toString();
-		String type = "D";
-		String operation = "Get parameters from Create Template API";
-		String message = "service_licence_type ==> " +service_licence_type.get(0) +" service_licence_instances ==> " +service_licence_instances.get(0) +"  service_licence_expiration_date " + service_licence_expiration_date.get(0) + "  service_licence_period ==>  " + service_licence_period.get(0);
-		String status = "";
-		logger.debug(
-				"{\"type\":\"{}\",\"timestamp\":\"{}\",\"start_stop\":\"\",\"component\":\"tng-sla-mgmt\",\"operation\":\"{}\",\"message\":\"{}\",\"status\":\"{}\",\"time_elapsed\":\"\"}",
-				type, timestamps, operation, message, status);
-		
+		System.out.println("service_licence_type ==> " + service_licence_type);
+		System.out.println("service_licence_instances ==> " + service_licence_instances);
+		System.out.println("service_licence_expiration_date ==> " + service_licence_expiration_date);
+		System.out.println("service_licence_period ==> " + service_licence_period);
+
+
 		// call CreateTemplate method
 		CreateTemplate ct = new CreateTemplate();
 		JSONObject template = ct.createTemplate(nsd_uuid.get(0), templateName.get(0), expireDate.get(0), guarantees,service_licence_type.get(0),service_licence_instances.get(0), service_licence_expiration_date.get(0),service_licence_period.get(0));
