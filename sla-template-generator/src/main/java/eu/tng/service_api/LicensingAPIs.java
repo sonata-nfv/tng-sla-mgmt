a9364fbc83f8403060695462c8006b67bb993149
Evgenia Kapassa <ekapassa@unipi.gr>
2018-12-04 13:15:47 +0200

    Add API for buying a license - fix parameters

--- sla-template-generator/src/main/java/eu/tng/service_api/LicensingAPIs.java
+++ sla-template-generator/src/main/java/eu/tng/service_api/LicensingAPIs.java
@@ -321,7 +321,14 @@ public class LicensingAPIs {
 		String allowed_instances = (String) LicenseinfoTemplate.get("allowed_instances");
 		String current_instances = "0";
 		
-		db_operations.createTableLicensing();		
+		System.out.println("LicenseinfoTemplate   " + LicenseinfoTemplate.toString());
+		
+		System.out.println("type " + license_type);
+		System.out.println("exp date " + license_exp_date);
+		System.out.println("period  " + license_period);
+		System.out.println("allowed " + allowed_instances);
+		
+		db_operations.createTableLicensing();
 		db_operations.insertLicenseRecord(sla_uuid.get(0), ns_uuid.get(0), "", cust_uuid.get(0), cust_email.get(0), license_type, license_exp_date, license_period, allowed_instances, current_instances, "", "");
 		
 		boolean update = db_operations.UpdateLicenseStatus(sla_uuid.get(0), ns_uuid.get(0), cust_uuid.get(0), "bought");
