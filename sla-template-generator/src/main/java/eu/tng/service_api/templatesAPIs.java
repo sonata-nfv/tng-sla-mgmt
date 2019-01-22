f630962efe0b8c115fe0cf7d3ce61c0c975fcab0
Evgenia Kapassa <ekapassa@unipi.gr>
2019-01-22 12:46:35 +0200

    catch exception if header is not included

--- sla-template-generator/src/main/java/eu/tng/service_api/templatesAPIs.java
+++ sla-template-generator/src/main/java/eu/tng/service_api/templatesAPIs.java
@@ -77,10 +77,34 @@ public class templatesAPIs {
 	@Produces(MediaType.APPLICATION_JSON)
 	@GET
 	public Response getTemplates(@Context HttpHeaders headers) {
-		
-		String Authorization = headers.getRequestHeader("Authorization").get(0);
-		System.out.println("Authorization token ---> " + Authorization);
-		
+
+		try {
+			String Authorization = headers.getRequestHeader("Authorization").get(0);
+			System.out.println("Authorization token ---> " + Authorization);
+			// logging
+			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			String timestamps = timestamp.toString();
+			String type = "I";
+			String operation = "Get Authorization token";
+			String message = "Authorization token feched succesfully! --> " + Authorization;
+			String status = String.valueOf(200);
+			logger.info(
+					"{\"type\":\"{}\",\"timestamp\":\"{}\",\"start_stop\":\"\",\"component\":\"tng-sla-mgmt\",\"operation\":\"{}\",\"message\":\"{}\",\"status\":\"{}\",\"time_elapsed\":\"\"}",
+					type, timestamps, operation, message, status);
+
+		} catch (Exception e) {
+			// logging
+			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			String timestamps = timestamp.toString();
+			String type = "W";
+			String operation = "Get Authorization token";
+			String message = "Error getting authorization token --> " + e;
+			String status = "";
+			logger.info(
+					"{\"type\":\"{}\",\"timestamp\":\"{}\",\"start_stop\":\"\",\"component\":\"tng-sla-mgmt\",\"operation\":\"{}\",\"message\":\"{}\",\"status\":\"{}\",\"time_elapsed\":\"\"}",
+					type, timestamps, operation, message, status);
+		}
+
 		ResponseBuilder apiresponse = null;
 		try {
 			String url = System.getenv("CATALOGUES_URL") + "slas/template-descriptors";
@@ -384,13 +408,16 @@ public class templatesAPIs {
 							sb.append(line + "\n");
 						}
 
-						// create correlation between ns and sla template among with licensing information
+						// create correlation between ns and sla template among with licensing
+						// information
 						JSONParser parser = new JSONParser();
 						createdTemplate = parser.parse(sb.toString());
 						JSONObject responseSLA = (JSONObject) createdTemplate;
 						String sla_uuid = (String) responseSLA.get("uuid");
 						ns_template_corr nstemplcorr = new ns_template_corr();
-						nstemplcorr.createNsTempCorr(nsd_uuid.get(0), sla_uuid, service_licence_type.get(0), service_licence_expiration_date.get(0), service_licence_period.get(0), allowed_service_instances.get(0), "inactive");
+						nstemplcorr.createNsTempCorr(nsd_uuid.get(0), sla_uuid, service_licence_type.get(0),
+								service_licence_expiration_date.get(0), service_licence_period.get(0),
+								allowed_service_instances.get(0), "inactive");
 
 						br.close();
 
