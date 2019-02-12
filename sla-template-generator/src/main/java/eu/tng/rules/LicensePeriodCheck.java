adff58bdb26ab7fa790dfbb436bb941b0ba470a2
Evgenia Kapassa <ekapassa@unipi.gr>
2019-02-12 12:00:42 +0200

    Add Listener for licensing period checks

---
+++ sla-template-generator/src/main/java/eu/tng/rules/LicensePeriodCheck.java
@@ -0,0 +1,99 @@
+/*
+ * 
+ *  Copyright (c) 2015 SONATA-NFV, 2017 5GTANGO [, ANY ADDITIONAL AFFILIATION]
+ *  ALL RIGHTS RESERVED.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ *  Neither the name of the SONATA-NFV, 5GTANGO [, ANY ADDITIONAL AFFILIATION]
+ *  nor the names of its contributors may be used to endorse or promote
+ *  products derived from this software without specific prior written
+ *  permission.
+ *  
+ *  This work has been performed in the framework of the SONATA project,
+ *  funded by the European Commission under Grant number 671517 through
+ *  the Horizon 2020 and 5G-PPP programmes. The authors would like to
+ *  acknowledge the contributions of their colleagues of the SONATA
+ *  partner consortium (www.sonata-nfv.eu).
+ *  
+ *  This work has been performed in the framework of the 5GTANGO project,
+ *  funded by the European Commission under Grant number 761493 through
+ *  the Horizon 2020 and 5G-PPP programmes. The authors would like to
+ *  acknowledge the contributions of their colleagues of the 5GTANGO
+ *  partner consortium (www.5gtango.eu).
+ * 
+ */
+
+package eu.tng.rules;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Application Lifecycle Listener implementation class LicensePeriodCheck
+ *
+ */
+public class LicensePeriodCheck implements ServletContextListener {
+
+	static Logger logger = LogManager.getLogger();
+
+	/**
+	 * Default constructor.
+	 */
+	public LicensePeriodCheck() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @contextDestroyed
+	 */
+	public void contextDestroyed(ServletContextEvent event) {
+		// logging
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		String timestamps = timestamp.toString();
+		String type = "I";
+		String operation = "License Check Listener";
+		String message = "[*] Listener License Check Stopped! - Restarting....";
+		String status = "";
+		logger.info(
+				"{\"type\":\"{}\",\"timestamp\":\"{}\",\"start_stop\":\"\",\"component\":\"tng-sla-mgmt\",\"operation\":\"{}\",\"message\":\"{}\",\"status\":\"{}\",\"time_elapsed\":\"\"}",
+				type, timestamps, operation, message, status);
+
+		contextInitialized(event);
+	}
+
+	/**
+	 * contextInitialized
+	 */
+	public void contextInitialized(ServletContextEvent arg0) {
+		System.out.println("[*] License Check Listener started!!");
+		
+		Date date=new Date();
+		Timer timer = new Timer();
+
+		timer.schedule(new TimerTask(){
+		     public void run(){
+		          System.out.println("Timer Running... " + new Date());
+		     }
+		},date, 60000);//24*60*60*1000 add 24 hours delay between job executions.  
+	
+	}
+
+}
