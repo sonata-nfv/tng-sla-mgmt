3ae3e0cf4c1c52161a1d10cf1a618c3e8bc9c216
Evgenia Kapassa <ekapassa@unipi.gr>
2019-05-06 13:50:39 +0300

    length fix in licenses

--- sla-template-generator/src/main/java/eu/tng/service_api/LicensingAPIs.java
+++ sla-template-generator/src/main/java/eu/tng/service_api/LicensingAPIs.java
@@ -80,7 +80,7 @@ public class LicensingAPIs {
 		dbo.closePostgreSQL();
 
 		apiresponse = Response.ok((Object) all_licenses);
-		apiresponse.header("Content-Length", all_licenses.toJSONString().length() - 4);
+		apiresponse.header("Content-Length", all_licenses.toJSONString().length());
 
 		// logging
 		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
