c2caa2a603ec1ecf074e45354c7aa07606077ad7
Evgenia Kapassa <ekapassa@unipi.gr>
2019-05-16 11:38:27 +0300

    send violation percentage to PG

--- sla-template-generator/src/main/java/eu/tng/rules/ListenerStatisticInfo.java
+++ sla-template-generator/src/main/java/eu/tng/rules/ListenerStatisticInfo.java
@@ -1,5 +1,6 @@
 package eu.tng.rules;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 
 import javax.servlet.ServletContextEvent;
@@ -7,6 +8,10 @@ import javax.servlet.ServletContextListener;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import eu.tng.correlations.db_operations;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Gauge;
 
 /**
  * Application Lifecycle Listener implementation class ListenerStatisticInfo
@@ -50,6 +55,64 @@ public class ListenerStatisticInfo implements ServletContextListener {
 				while (true) {
 					System.out.println("[*] statistic info run!! ");
 					// code for task to run ends here
+
+					// define the push gateway registry
+					CollectorRegistry registry = new CollectorRegistry();
+					
+					// open connection to database
+					db_operations db = new db_operations();
+					db_operations.connectPostgreSQL();
+					
+					/**
+					 * Nº SLA Agreements vs. Violations (in the last 24h / 7 days / 30 days) This
+					 * function is called every time an agreement is created and every time a
+					 * violation occurs call from > db_operations > insertRecordAgreement and >
+					 * db_operations > insertRecordViolation
+					 */
+					
+					double totalAgreements = db.countTotalAgreements();
+					double activeAgreements = db.countActiveAgreements();
+					double violatedAgreements = db.countViolatedAgreements();
+					double percentage_violated = 0;
+					double percentage_active = 0;
+
+					if (totalAgreements > 0) {
+						percentage_violated = (violatedAgreements * 100) / totalAgreements;
+						percentage_active = (activeAgreements * 100) / totalAgreements;
+					}
+
+					// create the gauge metric for the push gatewayl
+					Gauge gauge_percentage_violated = Gauge.build().name("percentage_violated_agreements")
+							.help("The percentage of the violated agreements").register(registry);
+					System.out.println("[*] gauge_percentage_violated ==> " + gauge_percentage_violated);
+					try {
+						// Set the value of the metric
+						gauge_percentage_violated.set(percentage_violated);
+						System.out.println("[*] gauge_percentage_violated ==> " + gauge_percentage_violated);
+						System.out.println("[*] gauge_percentage_violated with value ==> " + gauge_percentage_violated);
+
+					} finally {
+						// PushGateway pg = new PushGateway(System.getenv("MONITORING_PUSH_GATEWAY"));]
+						PushGateway pg = new PushGateway("http://pre-int-sp-ath.5gtango.eu:9091");
+						System.out.println("[*] pg => " + pg.toString());
+
+						try {
+							pg.pushAdd(registry, "SLA_job");
+							System.out.print("[*] Success! Statistic info pushed to PushGateway");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							System.out.println("[*] Error to push gateway => " + e);
+
+						}
+					}
+
+					System.out.println("[*] violationspercentageAll ==> " + percentage_violated);
+					
+					
+					
+					// close connection to databse
+					db_operations.closePostgreSQL();
+
 					try {
 						Thread.sleep(timeInterval);
 					} catch (InterruptedException e) {
